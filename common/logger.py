91bf8e959c9db0146f4d3120472d2a92761832f1
Marcelo Fornet <mfornet94@gmail.com>
2019-03-06 15:00:09 -0800

    Disable log to file temporary

--- common/logger.py
+++ common/logger.py
@@ -15,17 +15,17 @@ def add_logger(name, level):
     if not logger.handlers:
         formatter = logging.Formatter('%(message)s')
 
-        log_file = join(path, f'{name}.log')
-        
-        if not exists(path):
-            os.mkdir(path)
-            
-        if not exists(log_file):
-            open(log_file, 'a').close()
+        # log_file = join(path, f'{name}.log')
 
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+        # if not exists(path):
+        #     os.mkdir(path)
+
+        # if not exists(log_file):
+        #     open(log_file, 'a').close()
+
+        # file_handler = logging.FileHandler(log_file)
+        # file_handler.setFormatter(formatter)
+        # logger.addHandler(file_handler)
 
         stdout_handler = logging.StreamHandler(stdout)
         stdout_handler.setFormatter(formatter)
